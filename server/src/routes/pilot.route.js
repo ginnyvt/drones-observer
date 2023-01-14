d8352a58238a020210e998ab7624582433ac7099
ginnyvt <ginny@powerkernel.com>
2023-01-14 13:47:18 +0200

    chore: rename app endpoint

--- server/src/routes/pilot.route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import pilotController from "../controllers/pilot.controller.js";
-const router = Router();
-
-router.get("/pilots", pilotController.getPilots);
-
-export default router;
